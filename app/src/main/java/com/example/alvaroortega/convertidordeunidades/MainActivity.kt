1df199651d1cdef2e3286e3964e5320140e49dce
Alvarhito <aovargas05@gmail.com>
2018-04-12 00:59:47 -0500

    ac 1

--- app/src/main/java/com/example/alvaroortega/convertidordeunidades/MainActivity.kt
+++ app/src/main/java/com/example/alvaroortega/convertidordeunidades/MainActivity.kt
@@ -1,12 +1,8 @@
 package com.example.alvaroortega.convertidordeunidades
 
-import android.content.Intent
 import android.graphics.Color
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.os.Handler
-import android.util.Log
-import android.view.View
 import android.widget.Button
 import com.github.kittinunf.fuel.android.core.Json
 import kotlinx.android.synthetic.main.activity_main.*
@@ -18,8 +14,22 @@ import org.json.JSONObject
 import java.util.*
 
 class MainActivity : AppCompatActivity() {
+
     var botones1=arrayListOf<Button>()
     var botones2=arrayListOf<Button>()
+
+    var binario=false
+    var binario2=false
+
+    var hexadecimal=false
+    var hexadecimal2=false
+
+    var octal=false
+    var octal2=false
+
+    var decimal=false
+    var decimal2=false
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -30,6 +40,51 @@ class MainActivity : AppCompatActivity() {
         botones()
         getJson()
 
+        Covertir.setOnClickListener{
+            verificar()
+        }
+
+    }
+
+    fun verificar(){
+        val aConvertir = resultado.getText().toString()
+        if(binario && octal2){
+            bin_oct(aConvertir)
+
+        }else if(binario && hexadecimal2){
+            bin_hex(aConvertir)
+
+        }else if(binario && decimal2){
+            bin_dec(aConvertir)
+
+        }else if(octal && binario2){
+            oct_bin(aConvertir)
+
+        }else if(octal && hexadecimal2){
+            oct_hex(aConvertir)
+
+        }else if(octal && decimal2) {
+            oct_dec(aConvertir)
+
+        }else if(hexadecimal && binario2){
+            hex_bin(aConvertir)
+
+        }else if(hexadecimal && octal2){
+            hex_oct(aConvertir)
+
+        }else if(hexadecimal && decimal2){
+            hex_dec(aConvertir)
+
+        }else if(decimal && binario2){
+            dec_bin(aConvertir)
+
+        }else if(decimal && octal2){
+            dec_oct(aConvertir)
+
+        }else if(decimal && hexadecimal2){
+            dec_hex(aConvertir)
+
+        }
     }
 
     fun botones(){
@@ -49,24 +104,44 @@ class MainActivity : AppCompatActivity() {
             Hexadecimal.setBackgroundColor(Color.GRAY)
             Octal.setBackgroundColor(Color.GRAY)
             Decimal.setBackgroundColor(Color.GRAY)
+
+            binario=true
+            hexadecimal=false
+            octal=false
+            decimal=false
         }
         Binario2.setOnClickListener{
             Binario2.setBackgroundColor(Color.GREEN)
             Hexadecimal2.setBackgroundColor(Color.GRAY)
             Octal2.setBackgroundColor(Color.GRAY)
             Decimal2.setBackgroundColor(Color.GRAY)
+
+            binario2=true
+            hexadecimal2=false
+            octal2=false
+            decimal2=false
         }
         Hexadecimal.setOnClickListener{
             Binario.setBackgroundColor(Color.GRAY)
             Hexadecimal.setBackgroundColor(Color.GREEN)
             Octal.setBackgroundColor(Color.GRAY)
             Decimal.setBackgroundColor(Color.GRAY)
+
+            binario=false
+            hexadecimal=true
+            octal=false
+            decimal=false
         }
         Hexadecimal2.setOnClickListener{
             Binario2.setBackgroundColor(Color.GRAY)
             Hexadecimal2.setBackgroundColor(Color.GREEN)
             Octal2.setBackgroundColor(Color.GRAY)
             Decimal2.setBackgroundColor(Color.GRAY)
+
+            binario2=false
+            hexadecimal2=true
+            octal2=false
+            decimal2=false
 
         }
         Octal.setOnClickListener{
@@ -75,6 +150,11 @@ class MainActivity : AppCompatActivity() {
             Octal.setBackgroundColor(Color.GREEN)
             Decimal.setBackgroundColor(Color.GRAY)
 
+            binario=false
+            hexadecimal=false
+            octal=true
+            decimal=false
+
         }
         Octal2.setOnClickListener{
             Binario2.setBackgroundColor(Color.GRAY)
@@ -82,23 +162,38 @@ class MainActivity : AppCompatActivity() {
             Octal2.setBackgroundColor(Color.GREEN)
             Decimal2.setBackgroundColor(Color.GRAY)
 
+            binario2=false
+            hexadecimal2=false
+            octal2=true
+            decimal2=false
+
         }
         Decimal.setOnClickListener{
             Binario.setBackgroundColor(Color.GRAY)
             Hexadecimal.setBackgroundColor(Color.GRAY)
             Octal.setBackgroundColor(Color.GRAY)
             Decimal.setBackgroundColor(Color.GREEN)
+
+            binario=false
+            hexadecimal=false
+            octal=false
+            decimal=true
         }
         Decimal2.setOnClickListener{
             Binario2.setBackgroundColor(Color.GRAY)
             Hexadecimal2.setBackgroundColor(Color.GRAY)
             Octal2.setBackgroundColor(Color.GRAY)
             Decimal2.setBackgroundColor(Color.GREEN)
+
+            binario2=false
+            hexadecimal2=false
+            octal2=false
+            decimal2=true
         }
     }
     fun getJson() {
         val random = Random()
-        var num = random.nextInt(20 -1)+1
+        var num = random.nextInt(90 -1)+1
         var url="https://jsonplaceholder.typicode.com/posts/"
         url+=num
         //an extension over string (support GET, PUT, POST, DELETE with httpGet(), httpPut(), httpPost(), httpDelete())
@@ -120,7 +215,7 @@ class MainActivity : AppCompatActivity() {
     }
     fun Organizar(info: Json){
         var aux: JSONObject = JSONObject()
-        
+
         aux=info.obj()
         try {
             var auxi1=aux.getString("title")
@@ -130,5 +225,85 @@ class MainActivity : AppCompatActivity() {
         }catch (e: JSONException){
             e.printStackTrace()
         }
+    }
+
+    fun dec_bin(valor: String) {
+        var valor = valor
+        val dec = Integer.parseInt(valor)
+        valor = Integer.toBinaryString(dec)
+        prueba.text="Resultado: "+valor.toString()
+    }
+
+    fun dec_hex(valor: String) {
+        var valor = valor
+        val dec = Integer.parseInt(valor)
+        valor = Integer.toHexString(dec)
+        prueba.text="Resultado: "+valor.toString()
+    }
+
+    fun dec_oct(valor: String) {
+        var valor = valor
+        val dec = Integer.parseInt(valor)
+        valor = Integer.toOctalString(dec)
+        prueba.text="Resultado: "+valor.toString()
+    }
+
+    fun bin_dec(valor: String) {
+
+        val dec = Integer.parseInt(valor, 2)
+        prueba.text="Resultado: "+dec.toString()
+    }
+
+    fun bin_hex(valor: String) {
+        var valor = valor
+        val dec = Integer.parseInt(valor, 2)
+        valor = Integer.toHexString(dec)
+        prueba.text="Resultado: "+valor.toString()
+    }
+
+    fun bin_oct(valor: String) {
+        var valor = valor
+        val dec = Integer.parseInt(valor, 2)
+        valor = Integer.toOctalString(dec)
+        prueba.text="Resultado: "+valor.toString()
+    }
+
+    fun hex_dec(valor: String) {
+        val dec = Integer.parseInt(valor, 16)
+        prueba.text="Resultado: "+valor.toString()
+    }
+
+    fun hex_bin(valor: String) {
+        var valor = valor
+        val dec = Integer.parseInt(valor, 16)
+        valor = Integer.toBinaryString(dec)
+        prueba.text="Resultado: "+valor.toString()
+    }
+
+    fun hex_oct(valor: String) {
+        var valor = valor
+        val dec = Integer.parseInt(valor, 16)
+        valor = Integer.toOctalString(dec)
+        prueba.text="Resultado: "+valor.toString()
+    }
+
+    fun oct_dec(valor: String) {
+        val dec = Integer.parseInt(valor, 8)
+
+        prueba.text="Resultado: "+valor.toString()
+    }
+
+    fun oct_bin(valor: String) {
+        var valor = valor
+        val dec = Integer.parseInt(valor, 8)
+        valor = Integer.toBinaryString(dec)
+        prueba.text="Resultado: "+valor.toString()
+    }
+
+    fun oct_hex(valor: String) {
+        var valor = valor
+        val dec = Integer.parseInt(valor, 8)
+        valor = Integer.toHexString(dec)
+        prueba.text="Resultado: "+valor.toString()
     }
 }
